47c438fa4e26376ea47c10cf16733e1e1bca4030
CHAITHANYA4523 <116330280+CHAITHANYA4523@users.noreply.github.com>
2023-10-16 22:07:28 +0530

    Time: 7 ms (8.44%), Space: 8.6 MB (12.36%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    void totalPermutations(vector<vector<int>>& ans, vector<int>& nums, int freq[], vector<int>ds){
+    void totalPermutations(vector<vector<int>>& ans, vector<int>& nums, vector<int> ds, int index){
         //base case
-        if(ds.size() == nums.size()){ //permutation ds size is equal to nums
+        if(index == nums.size()){ //permutation ds size is equal to nums
+            for(int i=0; i<nums.size(); i++) ds.push_back(nums[i]);
             ans.push_back(ds);
             return;
         }
-        for(int i=0; i<nums.size(); i++){
-            if(!freq[i]){
-                freq[i] = 1;
-                ds.push_back(nums[i]);
-                totalPermutations(ans, nums, freq, ds);
-                ds.pop_back();
-                freq[i] = 0;
-            }
+        for(int i=index; i<nums.size(); i++){
+            swap(nums[index], nums[i]);
+            totalPermutations(ans, nums, ds, index+1);
+            swap(nums[i], nums[index]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
         int n = nums.size();
-        int freq[n];
-        for(int i=0; i<nums.size(); i++) freq[i] = 0;
-        totalPermutations(ans, nums, freq, ds);
+        vector<int> ds;
+        totalPermutations(ans, nums, ds, 0);
         return ans;
     }
 };
