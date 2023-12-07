e9594efc389598d98c3bf711fb7b77f5f08ae322
CHAITHANYA4523 <116330280+CHAITHANYA4523@users.noreply.github.com>
2023-12-07 23:15:21 +0530

    Time: 8 ms (96.38%), Space: 21.6 MB (18.43%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -11,25 +11,24 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* root, long long targetSum, long long sum, int &totalPaths){
-        if(root == NULL) return;
-        sum += root->val;
-        if(sum == targetSum){
-            totalPaths++;
-        }
-        dfs(root->left, targetSum, sum, totalPaths);
-        dfs(root->right, targetSum, sum, totalPaths);
-    }
-    void noOfPossiblePaths(TreeNode* root, long long targetSum, long long sum, int &totalPaths){
-        if(root == NULL) return;
-        dfs(root, targetSum, sum, totalPaths);
-        noOfPossiblePaths(root->left, targetSum, 0, totalPaths);
-        noOfPossiblePaths(root->right, targetSum, 0, totalPaths);
+    long long ans = 0, sum;
+    unordered_map<long long,long long> visited;
+    void dfs(TreeNode* &root, long long currentSum){
+        currentSum += root->val;
+        // checking if we already found an interval summing up to that
+        ans += visited[currentSum - sum];
+        visited[currentSum]++;
+        if (root->left) dfs(root->left, currentSum);
+        if (root->right) dfs(root->right, currentSum);
+        // clearing visited of the last value, for backtracking purposes
+        visited[currentSum]--;
     }
     int pathSum(TreeNode* root, int targetSum) {
-        if(root == NULL) return 0;
-        int totalPaths = 0;
-        noOfPossiblePaths(root, targetSum, 0, totalPaths);
-        return totalPaths;
+        sum = targetSum;
+        // visited set with initial value 0 will help us match from the root
+        visited[0] = 1;
+        if(root == NULL) return ans;
+        dfs(root, 0);
+        return ans;
     }
 };
