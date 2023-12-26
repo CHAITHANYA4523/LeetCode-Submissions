7f2ab771f8e9fe399bd3a2af7aa5de904b01b3bd
Chaithanya <116330280+CHAITHANYA4523@users.noreply.github.com>
2023-12-26 23:15:52 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -4,50 +4,26 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-//   private:
-//     void dfs(int V, vector<int> adj[], bool &ans, vector<int> &visited, int node){
-//         visited[node] = 1;
-//         for(auto it:adj[node]){
-//             if(!visited[it]){
-//                 dfs(V, adj, ans, visited, it);
-//             }
-//             else{
-//                 ans = true;
-//                 break;
-//             }
-//         }
-//     }
-
-private:
-    bool detectCycle(int src, vector<int> adj[], int visited[]){
-        queue<pair<int,int>> q;
-        visited[src] = 1;
-        q.push({src, -1});
-        while(!q.empty()){
-            int node = q.front().first;
-            int parent = q.front().second;
-            q.pop();
-            
-            for(auto adjacentNode:adj[node]){
-                if(!visited[adjacentNode]){
-                    visited[adjacentNode] = 1;
-                    q.push({adjacentNode, node});
-                }
-                // 
-                else if(parent != adjacentNode){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+  bool detectCycle(int src, int parent, int visited[], vector<int> adj[]){
+      visited[src] = 1;
+      for(auto adjacentNode:adj[src]){
+          if(!visited[adjacentNode]){
+            if(detectCycle(adjacentNode, src, visited, adj) == true) return true;  
+          }
+          //already visited node's parent is not current parent(adjacentNode)
+          else if(adjacentNode != parent){
+             return true;
+          }
+      }   
+      return false;
+  }
   public:
     // Function to detect cycle in an undirected graph.
     bool isCycle(int V, vector<int> adj[]) {
         int visited[V] = {0};
         for(int i=0; i<V; i++){
             if(!visited[i]){
-                if(detectCycle(i, adj, visited) == true) return true;
+                if(detectCycle(i, -1, visited, adj) == true) return true;
             }
         }
         return false;
