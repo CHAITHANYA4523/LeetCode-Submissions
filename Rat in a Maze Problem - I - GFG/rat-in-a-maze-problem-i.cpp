30eaf17d1aef19e89a73d72c9d6a79c83b0e067c
Chaithanya <116330280+CHAITHANYA4523@users.noreply.github.com>
2023-12-27 22:08:06 +0530

    Added solution - LeetHub

---
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution{
+    private:
+    void dfs(vector<vector<int>> &m, vector<string> &res, string validPath, vector<vector<int>> &vis, int row, int col){
+        vis[row][col] = 1;
+        int rowpath[] = {-1, 0, 1, 0};
+        int colpath[] = {0, 1, 0, -1};
+        int n = m.size();
+        if(row == n-1 && col == n-1){
+            res.push_back(validPath);
+        }
+        for(int i=0; i<4; i++){
+            int nrow = row + rowpath[i];
+            int ncol = col + colpath[i];
+            if(nrow >= 0 && nrow < n && ncol >=0 && ncol < n){
+                if(!vis[nrow][ncol] && m[nrow][ncol] == 1){
+                    string direction;
+                    if(i == 0) direction = "U";
+                    else if(i == 1) direction = "R";
+                    else if(i == 2) direction = "D";
+                    else direction = "L";
+                    dfs(m, res, validPath + direction, vis, nrow, ncol);
+                }
+            }
+        }
+        //back track
+        vis[row][col] = 0;
+    }
+    public:
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        vector<string> res;
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+        string validPath = "";
+        //if starting cell is not bolcked
+        if(m[0][0] == 1) dfs(m, res, validPath, vis, 0, 0);
+        if(res.empty()) return {"-1"};
+        sort(res.begin(), res.end());
+        return res;
+    }
+};
+
+    
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> m(n, vector<int> (n,0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> m[i][j];
+            }
+        }
+        Solution obj;
+        vector<string> result = obj.findPath(m, n);
+        sort(result.begin(), result.end());
+        if (result.size() == 0)
+            cout << -1;
+        else
+            for (int i = 0; i < result.size(); i++) cout << result[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
