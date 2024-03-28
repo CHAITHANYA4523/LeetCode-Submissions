88567de3660d3e516fc6bd8014b449d45fe433a5
Chaithanya <116330280+CHAITHANYA4523@users.noreply.github.com>
2024-03-28 23:44:28 +0530

    Time: 437 ms (38.73%), Space: 209.6 MB (41.78%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,31 +1,58 @@
 class Solution {
-private:
-    int memoisation(vector<int>& prices, int ind, int canBuy,
-    vector<vector<vector<int>>> &dp, int transactions){
-        if(ind == prices.size()){
-            return 0;
-        }
-        if(transactions == 0){
-            return 0; 
-        }
-        if(dp[ind][canBuy][transactions] != -1) return dp[ind][canBuy][transactions];
-        long long profit = 0;
-        if(canBuy){
-            int buy = -prices[ind] + memoisation(prices, ind+1, 0, dp, transactions);
-            int notBuy = memoisation(prices, ind+1, 1, dp, transactions);
-            profit = max(buy, notBuy);
-        }
-        else{
-            int sell = prices[ind] + memoisation(prices, ind+1, 1, dp, transactions-1);
-            int notSell = memoisation(prices, ind+1, 0, dp, transactions);
-            profit = max(sell, notSell);
-        }
-        return dp[ind][canBuy][transactions] = profit;
-    }
+// private:
+//     int memoisation(vector<int>& prices, int ind, int canBuy,
+//     vector<vector<vector<int>>> &dp, int transactions){
+//         if(ind == prices.size()){
+//             return 0;
+//         }
+//         if(transactions == 0){
+//             return 0; 
+//         }
+//         if(dp[ind][canBuy][transactions] != -1) return dp[ind][canBuy][transactions];
+//         long long profit = 0;
+//         if(canBuy){
+//             int buy = -prices[ind] + memoisation(prices, ind+1, 0, dp, transactions);
+//             int notBuy = memoisation(prices, ind+1, 1, dp, transactions);
+//             profit = max(buy, notBuy);
+//         }
+//         else{
+//             int sell = prices[ind] + memoisation(prices, ind+1, 1, dp, transactions-1);
+//             int notSell = memoisation(prices, ind+1, 0, dp, transactions);
+//             profit = max(sell, notSell);
+//         }
+//         return dp[ind][canBuy][transactions] = profit;
+//     }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
-        return memoisation(prices, 0, 1, dp, 2);
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
+        //base cases
+        // for(int i=0; i<2; i++){
+        //     for(int j=0; j<3; j++){
+        //         dp[n][i][j] = 0;
+        //     }
+        // }
+        // for(int i=0; i<=n; i++){
+        //     for(int j=0; j<2; j++){
+        //         dp[i][j][0] = 0;
+        //     }
+        // }
+        for(int i=n-1; i>=0; i--){
+            for(int j=0; j<=1; j++){
+                for(int k=1; k<=2; k++){
+                    if(j==1){
+                        int buy = -prices[i] + dp[i+1][0][k];
+                        int notBuy = dp[i+1][1][k];
+                        dp[i][j][k] = max(buy, notBuy);
+                    }
+                    else{
+                        int sell = prices[i] + dp[i+1][1][k-1];
+                        int notSell = dp[i+1][0][k];
+                        dp[i][j][k] = max(sell, notSell);
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
